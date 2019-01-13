de3b8033cca6191de8297066efebe35e9035d451
Anwesh43 <anweshthecool0@gmail.com>
2019-01-13 19:50:36 +0530

    adding drawDSPNode drawing method

--- DoubleSidePStage.ts
+++ DoubleSidePStage.ts
@@ -6,6 +6,7 @@ const scDiv : number = 0.51
 const strokeFactor : number = 90
 const color : string = "#673AB7"
 const backColor : string = "#BDBDBD"
+const sizeFactor : number = 3
 
 const maxScale : Function = (scale : number, i : number, n : number) : number => {
     return Math.max(0, scale - i / n)
@@ -20,4 +21,48 @@ const mirrorValue : Function = (scale : number, a : number, b : number) => {
 }
 const updateScale : Function = (scale : number, dir : number, a : number, b : number) : number => {
     return mirrorValue(scale, a, b) * dir * scGap
+}
+
+const drawArc : Function = (context : CanvasRenderingContext2D, y : number, r : number, start : number, end : number, scale : number) => {
+    var deg : number = start + (end - start) * scale
+    var k : number = 0
+    var dir = 1
+    if (start > end) {
+        dir = -1
+    }
+    context.save()
+    context.translate(0, y)
+    context.beginPath()
+    context.moveTo(r * Math.cos(start * Math.PI/180), r * Math.sin(start * Math.PI/180))
+    while (k != deg) {
+        context.lineTo(r * Math.cos(k * Math.PI/180), r * Math.cos(k * Math.PI/180))
+        k += dir
+    }
+    context.stroke()
+    context.restore()
+}
+
+const drawDSPNode : Function = (context : CanvasRenderingContext2D, i : number, scale : number) => {
+    const gap : number = w / (nodes + 1)
+    const size : number = gap / sizeFactor
+    context.strokeStyle = color
+    context.lineCap = 'round'
+    context.lineWidth = Math.min(w, h) / strokeFactor
+    const sc1 : number = divideScale(scale, 0, 2)
+    const sc2 : number = divideScale(scale, 1, 2)
+    const r : number = size / noOfP
+    var deg : number = -90
+    context.save()
+    context.translate(gap * (i + 1), h / 2)
+    context.beginPath()
+    context.moveTo(0, -size)
+    context.lineTo(0, size)
+    context.stroke()
+    for (var j = 0; j < noOfP; j++) {
+        const si : number = j % 2
+        const sf : number = 1 - 2 * si
+        const sc : number = divideScale(scale, j, noOfP)
+        drawArc(context, -size + r + r * j, r, deg, deg + 180 * sf, sc * sf)
+    }
+    context.restore()
 }
