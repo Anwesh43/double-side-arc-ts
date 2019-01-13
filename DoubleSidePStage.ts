32c6d0b7c98970c739bf2f40c81d9acd3052f27b
Anwesh43 <anweshthecool0@gmail.com>
2019-01-13 19:09:03 +0530

    adding constants

---
+++ DoubleSidePStage.ts
@@ -0,0 +1,8 @@
+const w : number = window.innerWidth, h : number = window.innerHeight
+const nodes : number = 5
+const noOfP : number = 4
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const strokeFactor : number = 90
+const color : string = "#673AB7"
+const backColor : string = "#BDBDBD"
