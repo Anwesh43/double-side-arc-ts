1244367290606560f7519aa711b02aedd9852297
Anwesh43 <anweshthecool0@gmail.com>
2019-01-13 19:27:43 +0530

    adding scale methods

--- DoubleSidePStage.ts
+++ DoubleSidePStage.ts
@@ -6,3 +6,18 @@ const scDiv : number = 0.51
 const strokeFactor : number = 90
 const color : string = "#673AB7"
 const backColor : string = "#BDBDBD"
+
+const maxScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.max(0, scale - i / n)
+}
+const divideScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.min(1/n, scale - i / n) * n
+}
+const scaleFactor : Function = (scale : number) : number => Math.floor(scale / scDiv)
+const mirrorValue : Function = (scale : number, a : number, b : number) => {
+    const k : number = scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+const updateScale : Function = (scale : number, dir : number, a : number, b : number) : number => {
+    return mirrorValue(scale, a, b) * dir * scGap
+}
