a28ae8a1e970a3a683182ee8e9499bdf981d840a
Anwesh43 <anweshthecool0@gmail.com>
2019-01-13 20:04:49 +0530

    adding DSPNode class

--- DoubleSidePStage.ts
+++ DoubleSidePStage.ts
@@ -138,3 +138,47 @@ class Animator {
         }
     }
 }
+
+class DSPNode {
+    prev : DSPNode
+    next : DSPNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new DSPNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        drawDSPNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : DSPNode {
+        var curr : DSPNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
