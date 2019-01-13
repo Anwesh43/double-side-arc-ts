d07d6b1a99380826c0f480c4b4b1a61d3fe0f0ad
Anwesh43 <anweshthecool0@gmail.com>
2019-01-13 20:15:41 +0530

    using renderer in Stage class

--- DoubleSidePStage.ts
+++ DoubleSidePStage.ts
@@ -70,6 +70,7 @@ const drawDSPNode : Function = (context : CanvasRenderingContext2D, i : number, 
 class DoubleSidePStage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
+    renderer : Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w
@@ -81,11 +82,14 @@ class DoubleSidePStage {
     render() {
         this.context.fillStyle = backColor
         this.context.fillRect(0, 0, w, h)
+        this.renderer.render(this.context)
     }
 
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
